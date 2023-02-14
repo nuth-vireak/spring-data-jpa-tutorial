f5651f1ac27e4519cf2bdf3e37577d95280ec95a
nuth-vireak <timberr.4t@gmail.com>
2023-02-15 00:08:24 +0700

    // TODO: 5:00:00 Understanding Repositories and Methods

--- src/main/java/com/dailycodebuffer/spring/data/jpatutorial/Entity/Student.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpatutorial/Entity/Student.java
@@ -1,26 +1,46 @@
 package com.dailycodebuffer.spring.data.jpatutorial.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailid_unique",
+                columnNames = "email_address"
+        )
+)
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Builder
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
     private String emailId;
     private String guardianName;
     private String guardianEmail;
     private String guardianMobile;
 
-    // TODO: 4:52:00 Different JPA annotations
+    // TODO: 5:00:00 Understanding Repositories and Methods
 }

b37eed3cea1e460bb17672fc87bea8b65b18277a
nuth-vireak <timberr.4t@gmail.com>
2023-02-20 16:31:16 +0700

    Create Course and CourseMaterial Entity

---
+++ src/main/java/com/dailycodebuffer/spring/data/jpatutorial/entity/Course.java
@@ -0,0 +1,30 @@
+package com.dailycodebuffer.spring.data.jpatutorial.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Course {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence"
+    )
+    private Long courseId;
+    private String title;
+    private Integer credit;
+
+}
---
+++ src/main/java/com/dailycodebuffer/spring/data/jpatutorial/entity/CourseMaterial.java
@@ -0,0 +1,35 @@
+package com.dailycodebuffer.spring.data.jpatutorial.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class CourseMaterial {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_material_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(
+            name = "course_id",
+            referencedColumnName = "courseId"
+    )
+    private Course course;
+}
